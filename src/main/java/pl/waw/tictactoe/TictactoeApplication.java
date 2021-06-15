faa484dea1a9a24c8700c306b2e5aab7d3ca9ac5
jakub <jakub.wawrzenczak@gmail.com>
2021-06-15 22:54:22 +0200

    Enabled JPA repositories

--- src/main/java/pl/waw/tictactoe/TictactoeApplication.java
+++ src/main/java/pl/waw/tictactoe/TictactoeApplication.java
@@ -2,7 +2,9 @@ package pl.waw.tictactoe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class TictactoeApplication {
 
